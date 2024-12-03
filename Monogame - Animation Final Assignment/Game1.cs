2ab6d8ea64fb90adf2caafa1bd4a6287179be082
Ashton Hotke <339741886@GoTVDSB.ca>
2024-12-03 09:31:05 -0500

    close

--- Monogame - Animation Final Assignment/Game1.cs
+++ Monogame - Animation Final Assignment/Game1.cs
@@ -22,8 +22,13 @@ namespace Monogame___Animation_Final_Assignment
 
         MouseState mouseState;
 
-        float seconds, timeElapsed;
+        float seconds = 0f;
 
+        bool timing = false;
+
+        Texture2D introScreenTexture, mainBgTexture, endScreenTexture;
+
+        Rectangle backgroundRect;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -34,6 +39,11 @@ namespace Monogame___Animation_Final_Assignment
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 500;
+            _graphics.ApplyChanges();
+
             screen = Screen.start;
             base.Initialize();
         }
@@ -43,6 +53,12 @@ namespace Monogame___Animation_Final_Assignment
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+            backgroundRect = new Rectangle(0, 0, 800, 500);
+
+            introScreenTexture = Content.Load<Texture2D>("introScreen");
+            mainBgTexture = Content.Load<Texture2D>("Pizzeria");
+            endScreenTexture = Content.Load<Texture2D>("endScreen");
         }
 
         protected override void Update(GameTime gameTime)
@@ -51,6 +67,9 @@ namespace Monogame___Animation_Final_Assignment
                 Exit();
 
             // TODO: Add your update logic here
+
+            this.Window.Title = $"x = {mouseState.X}, y = {mouseState.Y}" + "     " + seconds;
+            // this.Window.Title = "The Food Fight";
             mouseState = Mouse.GetState();
 
             if (screen == Screen.start)
@@ -58,26 +77,59 @@ namespace Monogame___Animation_Final_Assignment
                 if (mouseState.LeftButton == ButtonState.Pressed)
                 {
                     screen = Screen.main;
+                    timing = true;
                 }
             }
 
             if (screen == Screen.main)
             {
-                Task.Delay(50000);
+                seconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+                if (seconds >= 18)
+                {
+                    screen = Screen.end;
+                    timing = false;
+                    seconds = 0;
+                }
 
             }
 
-            seconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (screen == Screen.end)
+            {
+                seconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+                if (seconds >= 6)
+                {
+                    Exit();
+                }
+            }
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Orange);
 
             // TODO: Add your drawing code here
 
+            _spriteBatch.Begin();
+
+            if (screen == Screen.start)
+            {
+                _spriteBatch.Draw(introScreenTexture, backgroundRect, Color.White);
+            }
+            if (screen == Screen.main)
+            {
+                _spriteBatch.Draw(mainBgTexture, backgroundRect, Color.White);
+            }
+
+            if (screen == Screen.end)
+            {
+                _spriteBatch.Draw(endScreenTexture, backgroundRect, Color.White);
+            }
+
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
